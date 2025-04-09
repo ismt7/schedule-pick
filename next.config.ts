26be2012b4859e34e6beefbaf39c636fa48dc771
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-04-09 17:23:58 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? "/schedule-pick" : "",
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
